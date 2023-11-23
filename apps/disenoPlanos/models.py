ff778c766ea84028aea2464e4454fc23c6dc3e1c
FernandoReyesV2 <freyesv2@unemi.edu.ec>
2023-11-23 02:41:53 -0500

    Cambios en el html, recibe imagen del plano y la guarda

--- apps/disenoPlanos/models.py
+++ apps/disenoPlanos/models.py
@@ -3,10 +3,7 @@ from PIL import Image
 from io import BytesIO
 from django.core.files.base import ContentFile
 
-# Create your models here.
 class Planos(models.Model):
-
-    # Atributo para guardar una imagen convertida a una secuencia de bytes
     imagen = models.ImageField(upload_to='planos/', blank=True, null=True)
 
     def __str__(self):
@@ -15,11 +12,12 @@ class Planos(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         if self.imagen:
-            # Llama a la función utilitaria para convertir la imagen a bytes
             byte_sequence = convertir_imagen_a_bytes(self.imagen.path)
             if byte_sequence:
+                # Borra la imagen original antes de guardar la nueva
+                self.imagen.delete(save=False)
                 # Actualiza el campo imagen con la secuencia de bytes
-                self.imagen.save('imagen.jpg', ContentFile(byte_sequence), save=False)
+                self.imagen.save('imagenes.jpg', ContentFile(byte_sequence), save=False)
                 super().save(*args, **kwargs)
 
 def convertir_imagen_a_bytes(imagen_path):
--- apps/disenoPlanos/views.py
+++ apps/disenoPlanos/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PlanosForm
 
-# Create your views here.
 def disenoPlanos(request):
-    return render(request, 'disenoPlanos.html')
+    if request.method == 'POST':
+        form = PlanosForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    else:
+        form = PlanosForm()
+    return render(request, 'disenoPlanos.html', {'form': form})
